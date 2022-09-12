338c0545db83a1ea57499ffdbbbe9a3e218e217d
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2022-09-12 17:36:04 +0100

    fetched data and set to table rows

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,41 @@
-
+// @ts-ignore
 const startApp = async () => {
-    
+    const url = 'https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84';
+    let response = null;
+    const tableBody = document.querySelector('tbody');
+    const nextButton = document.querySelectorAll('[data-foo]');
+
+    function getData(path = url) {
+        fetch(url).then((response) => {
+            if (!response.ok) throw new Error('An error occurred while fetching data.');
+            return response.json();
+        }).then(data => {
+            response = data.results[0];
+            populateTableData(response);
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+    getData();
+
+    function next() {
+        getData(response.paging.next);
+    }
+
+    function previous() {
+        getData(response.paging.previous);
+    }
+
+    function populateTableData(data) {
+        let rows = tableBody.children;
+        data['1'].forEach((item, index) => {
+            let cols = rows[index].children;
+            cols[0].innerHTML = item.row;
+            cols[1].innerHTML = item.gender;
+            cols[2].innerHTML = item.age;
+        });
+    }
 };
 
 document.addEventListener('DOMContentLoaded', startApp);
